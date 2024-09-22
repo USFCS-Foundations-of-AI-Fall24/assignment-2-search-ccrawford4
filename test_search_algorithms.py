11ad8d309305dd9fb120f03307c637943dce3400
ccrawford4 <calumcrawford9@gmail.com>
2024-09-21 22:50:29 -0700

    wrapped up all tests

--- test_search_algorithms.py
+++ test_search_algorithms.py
@@ -4,36 +4,40 @@ from search_algorithms import *
 
 
 class Test(TestCase):
+    action_list = [move_to_sample, pick_up_tool, use_tool, drop_tool,
+                   pick_up_sample, move_to_station, drop_sample,
+                   move_to_battery, charge]
+
     def test_breadth_first_search(self):
         def g(s):
             return s.loc == "battery"
         s = RoverState()
-        result = breadth_first_search(s, action_list, g)
+        result = breadth_first_search(s, self.action_list, g)
         print(result)
         def g2(s):
             return s.loc == "sample" and s.sample_extracted == True
         s2 = RoverState()
-        result = breadth_first_search(s2, action_list, g2)
+        result = breadth_first_search(s2, self.action_list, g2)
         print(result)
         def g3(s) :
             return s.charged == True and s.sample_extracted == True
         s3 = RoverState()
-        result = breadth_first_search(s3, action_list, g3)
+        result = breadth_first_search(s3, self.action_list, g3)
         print(result)
 
     def test_depth_first_search(self):
         def g(s):
             return s.loc == "battery"
         s = RoverState()
-        result = depth_first_search(s, action_list, g)
+        result = depth_first_search(s, self.action_list, g)
         print(result)
         def g2(s):
             return s.loc == "sample" and s.sample_extracted == True
         s2 = RoverState()
-        result = depth_first_search(s2, action_list, g2)
+        result = depth_first_search(s2, self.action_list, g2)
         print(result)
         def g3(s) :
             return s.charged == True and s.sample_extracted == True
         s3 = RoverState()
-        result = depth_first_search(s3, action_list, g3)
+        result = depth_first_search(s3, self.action_list, g3)
         print(result)
